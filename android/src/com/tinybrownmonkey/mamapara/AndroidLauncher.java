434ba61ee8999cc654674e6549ac130bf0988d32
= <pitbugx@yahoo.com>
2017-06-12 16:09:01 +1000

    Initial commit

---
+++ android/src/com/tinybrownmonkey/mamapara/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tinybrownmonkey.mamapara;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tinybrownmonkey.mamapara.MamaParaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MamaParaGame(), config);
+	}
+}
---
+++ desktop/src/com/tinybrownmonkey/mamapara/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tinybrownmonkey.mamapara.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tinybrownmonkey.mamapara.MamaParaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MamaParaGame(), config);
+	}
+}
